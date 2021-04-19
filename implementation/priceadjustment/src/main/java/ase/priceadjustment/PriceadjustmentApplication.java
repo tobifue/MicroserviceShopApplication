51d9e193d518cbf5426225b45fbdf0c1be8db7d8
Lukas Sorer <a01250600@unet.univie.ac.at>
2021-04-19 22:04:59 +0200

    added dummy price scraper that returns random values

--- implementation/priceadjustment/src/main/java/ase/priceadjustment/PriceadjustmentApplication.java
+++ implementation/priceadjustment/src/main/java/ase/priceadjustment/PriceadjustmentApplication.java
@@ -1,13 +1,35 @@
 package ase.priceadjustment;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import ase.priceadjustment.scraper.PriceScraper;
+
+@RestController
 @SpringBootApplication
 public class PriceadjustmentApplication {
 
+	private PriceScraper scraper;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PriceadjustmentApplication.class, args);
+	}
+
+	@PostMapping(path = "/recommend", consumes = "application/json", produces = "application/json")
+	public Double addTransaction(@RequestBody String item) {
+		return scraper.scrape(item);
+	}
+
+	@Bean
+	public CommandLineRunner initialize() {
+		return (args) -> {
+			this.scraper = new PriceScraper();
+		};
 	}
 
 }
