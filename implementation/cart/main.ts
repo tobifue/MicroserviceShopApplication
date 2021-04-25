6654c1ed8634e0a2aedcb43ae7c017d037000444
a01225308 <spliomixx@gmail.com>
2021-04-25 12:54:32 +0200

    change JS to tc

--- implementation/cart/main.ts
+++ implementation/cart/main.ts
@@ -1,5 +1,17 @@
-var Item = /** @class */ (function () {
-    function Item(itemId, itemName, quantity, price, vendorId, priceRecommendation) {
+
+
+
+
+
+
+class Item {
+    itemId: number;
+    itemName: string;
+    quantity: number;
+    price: number;
+    vendorId: string;
+    priceRecommendation: number;
+    constructor(itemId: number, itemName: string, quantity: number, price: number, vendorId: string, priceRecommendation: number) {
         this.itemId = itemId;
         this.itemName = itemName;
         this.quantity = quantity;
@@ -7,58 +19,77 @@ var Item = /** @class */ (function () {
         this.vendorId = vendorId;
         this.priceRecommendation = priceRecommendation;
     }
-    return Item;
-}());
-var Cart = /** @class */ (function () {
-    function Cart(id) {
+}
+
+
+class Cart {
+    customerId:number;
+    list:Item[];
+    constructor(id:number) {
         this.customerId = id;
         this.list = [];
     }
-    Cart.prototype.add = function (item) {
-        this.list.push(item);
+
+    add(item:Item) {
+        this.list.push(item)
         return this;
-    };
-    return Cart;
-}());
-var carts = new Map();
-var express = require('express');
-var app = express();
-var port = 8085;
+    }
+
+
+}
+
+
+
+let carts = new Map();
+
+const express = require('express')
+let app = express();
+const port = 8085;
 app.use(express.json());
+
+
+
+
 app.post('/addItem/:userId', function (req, res, next) {
-    var userId = req.params.userId;
-    var bdy = req.body;
-    var item = new Item(bdy.itemId, bdy.itemName, bdy.quantity, bdy.price, bdy.vendorId, bdy.priceRecommendation);
+    const userId = req.params.userId;
+    const bdy = req.body;
+    const item = new Item(bdy.itemId, bdy.itemName, bdy.quantity, bdy.price, bdy.vendorId, bdy.priceRecommendation);
     if (carts.has(userId)) {
         carts.get(userId).add(item);
-    }
-    else {
+    } else {
         carts.set(userId, (new Cart(userId)).add(item));
     }
     console.log("item: ", item);
     console.log("carts: ", carts.get(userId));
     res.send("OK");
 });
+
 app.get('/getCart/:userId', function (req, res, next) {
-    var userId = req.params.userId;
+
+    const userId = req.params.userId;
     console.log("get card called: ", carts);
     res.send(JSON.stringify(carts.get(userId)));
 });
+
 app.get('/deleteCart/:userId', function (req, res, next) {
-    var userId = req.params.userId;
+    const userId = req.params.userId;
     console.log("delete card called: ", carts);
-    carts["delete"](userId);
+    carts.delete(userId);
     res.send("OK");
 });
+
 app.listen(port, function () {
     console.log('Server started on port: ' + port);
 });
-var registration = {
-    "endpoints": ["/addItem", "/getCart", "/deleteCart"],
+
+
+let registration = {
+    "endpoints":["/addItem","/getCart", "/deleteCart"],
     "category": "cart",
     "ip": "http://localhost:8085"
-};
-var options = {
+}
+
+let options = {
     host: 'localhost',
     port: 8080,
     path: '/register/new',
@@ -66,16 +97,17 @@ var options = {
     headers: {
         "Content-Type": "application/json"
     }
-};
-var http = require('http');
-var httpreq2 = http.request(options, function (response) {
-    var data = "";
+}
+let http = require('http');
+let httpreq2 = http.request(options, function (response) {
+    let data = "";
     response.on('data', function (chunk) {
         data += chunk;
     });
     response.on('end', function () {
-        console.log(data);
-    });
+        console.log(data)
+    })
 });
 httpreq2.write(JSON.stringify(registration));
 httpreq2.end();
+
