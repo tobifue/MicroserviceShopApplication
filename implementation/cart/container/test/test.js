0f6c430182c82a57985e5c01e9bccc29558c0f3a
a01225308 <spliomixx@gmail.com>
2021-05-20 01:19:22 +0200

    test ci/cd

--- implementation/cart/container/test/test.js
+++ implementation/cart/container/test/test.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 const supertest= require("supertest");
 let api = supertest((process.env.GATEWAYIP || "localhost")+':8085');
-
+console.log("IP used: " + (process.env.GATEWAYIP || "localhost"));
 it('expect return a 200 respronse', function (done){
     api.get('/getCart/1')
     .set('Accept', 'application/json')
