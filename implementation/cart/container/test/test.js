74e2ddcaacc2eccc3a0a567460d1111cdf980d73
a01225308 <spliomixx@gmail.com>
2021-05-20 01:56:07 +0200

    sho ip

--- implementation/cart/container/test/test.js
+++ implementation/cart/container/test/test.js
@@ -3,6 +3,11 @@ var request = require('request');
 const supertest= require("supertest");
 let api = supertest((process.env.GATEWAYIP || "localhost")+':8085');
 console.log("IP used: " + (process.env.GATEWAYIP || "localhost"));
+
+
+var ip = require("ip");
+console.log(ip.address());
+
 it('expect return a 200 respronse', function (done){
     api.get('/getCart/1')
     .set('Accept', 'application/json')
