a4d4d743242a0af2fb00abb6cadff5247211df3f
a01225308 <spliomixx@gmail.com>
2021-05-21 20:01:40 +0200

    swap logic from gateway to checkout service

--- implementation/checkout/src/main/java/ase/checkout/controller/CheckoutController.java
+++ implementation/checkout/src/main/java/ase/checkout/controller/CheckoutController.java
@@ -1,16 +1,21 @@
 package ase.checkout.controller;
 
+import ase.checkout.util.NetworkUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.*;
-
-import ase.checkout.itemhandler.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,25 +25,49 @@ import java.util.Map;
 public class CheckoutController {
     private String userId;
     private String jsonCart;
-
-    @Value("${server.port}")
-    private String port = "8086";
+    private String gatewayIp = "http://localhost:8080";
+    private String checkoutAdress = "http://" + InetAddress.getLocalHost().getHostAddress() + ":8086";
 
     @RequestMapping(value = "/checkout/{costumerId}", method = RequestMethod.GET)
-    public String checkout(@PathVariable("costumerId") Long costumerId, Map<String, Object> cart) {
+    public String checkout(@PathVariable("costumerId") Long costumerId) {
         try {
-            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-            String json = ow.writeValueAsString(cart);
-            jsonCart = json;
+            //ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            //String json = ow.writeValueAsString(cart);
+            //jsonCart = json;
 
             System.out.println(costumerId);
         } catch (Exception e) {
             System.out.println("Something went wrong.");
         }
-        String outputJson = "{ \"command\":\"delete\"}";
-        doShipment();
-        saveTransactionInDb();
-        return outputJson;
+        String cart = NetworkUtil.httpGet(gatewayIp, String.format("/cart/getCart/%s", costumerId));
+        NetworkUtil.httpGet(gatewayIp, String.format("/cart/deleteCart/%s", costumerId));
+        try {
+            JSONObject jsonObject = new JSONObject(cart);
+            JSONArray jsonArray = jsonObject.getJSONArray("list");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                Map<String, Object> item = new ObjectMapper().readValue(jsonArray.getJSONObject(i).toString(), HashMap.class);
+                item.put("vendorId", costumerId.toString());
+                System.out.println(item); // display usernames
+                NetworkUtil.httpPost(gatewayIp, String.format("history/add", costumerId), item);
+                NetworkUtil.httpPost(gatewayIp, String.format("shipment/add", costumerId), item);
+            }
+            System.out.println("cart: " + cart);
+            doShipment();
+            saveTransactionInDb();
+        } catch (RestClientException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+        return "{ \"command\":\"delete\"}";
     }
 
     public void doShipment() {
@@ -46,23 +75,26 @@ public class CheckoutController {
 
     public void saveTransactionInDb() {
     }
-    public CheckoutController (){
+
+    public CheckoutController() throws UnknownHostException {
         registerWithGateway();
     }
-    //public CheckoutController(){registerWithGateway();}
+
+
     private void registerWithGateway() {
         try {
             Map<String, Object> registrationDetails = new HashMap<>();
             registrationDetails.put("endpoints", new ArrayList<String>() {
                 private static final long serialVersionUID = 1L;
+
                 {
                     // put highest level endpoints here
                     add("/checkout");
                 }
             });
             registrationDetails.put("category", "checkout");
-            registrationDetails.put("ip", "http://localhost:" + port);
-            new RestTemplate().postForObject(String.format("%s/%s", "http://localhost:8080", "/register/new"),
+            registrationDetails.put("ip", checkoutAdress);
+            new RestTemplate().postForObject(String.format("%s/%s", gatewayIp, "/register/new"),
                     registrationDetails, String.class);
         } catch (RestClientException e) {
             System.out.println("Could not reach Gateway, retrying in 5 seconds");
@@ -75,7 +107,6 @@ public class CheckoutController {
         }
         System.out.println("Successfully registered with gateway!");
     }
-
 
 
 }
--- implementation/gateway/src/main/java/ase/gateway/controller/CheckoutController.java
+++ implementation/gateway/src/main/java/ase/gateway/controller/CheckoutController.java
@@ -1,65 +1,24 @@
 package ase.gateway.controller;
 
 import ase.gateway.traffic.Message;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("checkout")
 public class CheckoutController {
 	private static final String serviceName = "checkout-service";
 
-	@GetMapping("/checkout/{customerId}")
-	@ResponseBody
-	public String checkout(@PathVariable Long customerId) {
+	@GetMapping(path = "/checkout/{customerId}", produces = "application/json")
+	public String checkout(@PathVariable("customerId") Long customerId) {
 		System.out.println("/checkout in CheckoutController is called with ID: " + customerId);
 		try {
-
-			//der Teil sollte ins Service!!!!!
-
-
-			String cart = TrafficController.sendMessageToSingleRecipient(
-					Message.createInstance(null, "cart", String.format("/getCart/%s", customerId), "GET"));
-
-			TrafficController.sendMessageToSingleRecipient(
-					Message.createInstance(null, "cart", String.format("/deleteCart/%s", customerId), "GET"));
-
-			System.out.println("/checkout in CheckoutController is called with ID: " + cart);
-
-
-			JSONObject jsonObject = new JSONObject(cart);
-			JSONArray jsonArray = jsonObject.getJSONArray("list");
-			for (int i = 0; i < jsonArray.length(); i++) {
-				Map<String, Object> item = new ObjectMapper().readValue(jsonArray.getJSONObject(i).toString(), HashMap.class);
-				item.put("vendorId", customerId.toString());
-				System.out.println(item); // display usernames
-				TrafficController.sendMessageToSingleRecipient(
-						Message.createInstance(item, "history", String.format("/add", customerId), "POST"));
-				TrafficController.sendMessageToSingleRecipient(
-						Message.createInstance(item, "shipment", String.format("/add", customerId), "POST"));
-			}
-
 			return TrafficController.sendMessageToSingleRecipient(
 					Message.createInstance(null, "checkout", String.format("/checkout/%s", customerId), "GET"));
 		} catch (RestClientException e) {
-			e.printStackTrace();
-			return e.getMessage();
-		} catch (JSONException e) {
-			e.printStackTrace();
-			return e.getMessage();
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-			return e.getMessage();
-		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 			return e.getMessage();
 		}
