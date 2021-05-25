eaf5fd62a6fbfd5c6471f1689aed73ff18ed2194
Kevin Miller <a01638797@unet.univie.ac.at>
2021-05-25 08:54:01 +0200

    refactored shipmentController for better testability

--- implementation/shipment/src/main/java/ase/shipment/controller/ShipmentController.java
+++ implementation/shipment/src/main/java/ase/shipment/controller/ShipmentController.java
@@ -9,16 +9,14 @@ import ase.shipment.data.ShipmentRepository;
 public class ShipmentController {
 
 	private Thread shipmentThread;
+	private ShipmentRepository repository;
 
 	public ShipmentController(ShipmentRepository repository) {
+		this.repository = repository;
 		shipmentThread = new Thread(() -> {
 			while (true) {
 				try {
-					List<Shipment> shipments = repository.findAll();
-					Shipment shipment = shipments.get(new Random().nextInt(shipments.size()));
-					System.out.println(shipment);
-					shipment.updateShippingStatus();
-					System.out.println("new status " + shipment.getShippingStatus());
+					updateRandomShipment();
 					Thread.sleep(10000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
@@ -26,6 +24,20 @@ public class ShipmentController {
 				}
 			}
 		});
+	}
+
+	private void updateRandomShipment() {
+		List<Shipment> shipments = repository.findAll();
+		Shipment shipment = shipments.get(new Random().nextInt(shipments.size()));
+		System.out.println(shipment);
+		shipment.updateShippingStatus();
+		System.out.println("new status " + shipment.getShippingStatus());
+	}
+
+	public void updateShipment(Shipment shipment) {
+		System.out.println(shipment);
+		shipment.updateShippingStatus();
+		System.out.println("new status " + shipment.getShippingStatus());
 	}
 
 	public void start() {
