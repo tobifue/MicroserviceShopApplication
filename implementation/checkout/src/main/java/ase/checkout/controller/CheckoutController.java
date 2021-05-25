96c87b8d49aa8842c47e51a784578e267fa034e7
a01225308 <spliomixx@gmail.com>
2021-05-25 14:12:06 +0200

    containerrized checkout

--- implementation/checkout/src/main/java/ase/checkout/controller/CheckoutController.java
+++ implementation/checkout/src/main/java/ase/checkout/controller/CheckoutController.java
@@ -80,7 +80,7 @@ public class CheckoutController {
         registerWithGateway();
     }
 
-
+    @RequestMapping(value = "/registerWithGateway", method = RequestMethod.GET)
     private void registerWithGateway() {
         try {
             Map<String, Object> registrationDetails = new HashMap<>();
@@ -96,16 +96,10 @@ public class CheckoutController {
             registrationDetails.put("ip", checkoutAdress);
             new RestTemplate().postForObject(String.format("%s/%s", gatewayIp, "/register/new"),
                     registrationDetails, String.class);
+            System.out.println("Successfully registered with gateway!");
         } catch (RestClientException e) {
-            System.out.println("Could not reach Gateway, retrying in 5 seconds");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e1) {
-                e1.printStackTrace();
-            }
-            registerWithGateway();
+            System.err.println("Failed to connect to Gateway, please register manually or restart application");
         }
-        System.out.println("Successfully registered with gateway!");
     }
 
 
