6f70ee8abe5f2af4af4e4f4549509126b99c52ac
Kevin Miller <a01638797@unet.univie.ac.at>
2021-06-04 10:37:22 +0200

    fixed markedproductservice

--- implementation/markedproduct/src/main/java/ase/markedproduct/controller/UpdateController.java
+++ implementation/markedproduct/src/main/java/ase/markedproduct/controller/UpdateController.java
@@ -3,6 +3,7 @@ package ase.markedproduct.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,34 +16,40 @@ public class UpdateController {
 		return new RestTemplate().getForObject(String.format("%s/%s", serviceAddress, endpoint), String.class);
 	}
 
-	public void updateProduct(Map<String, Object> updateMessage, MarkedProductRepository repository) {
+	public String updateProduct(Map<String, Object> updateMessage, MarkedProductRepository repository) {
 		List<MarkedProduct> markedProducts = repository.findAll();
+		String result = "Product not found";
 		for (MarkedProduct m : markedProducts) {
-			if (m.getVendorId().equals(updateMessage.get("vendorId"))
+
+			if (m.getVendorId() == Long.parseLong((String) updateMessage.get("vendorId"))
 					&& m.getItemName().equals(updateMessage.get("itemName"))) {
 
 				// check if price has changed
-				if (m.getPrice() < (double) updateMessage.get("price")) {
+				if (Double.parseDouble((String) updateMessage.get("price")) < m.getPrice()) {
 					System.out.println("Price alert triggered for product " + m.getItemName());
 					System.out.println("old price: " + m.getPrice() + "; new price: " + updateMessage.get("price"));
-
+					result = String.format("price changed from %s to %s", m.getPrice(), updateMessage.get("price"));
 					// send notification
 					String gatewayIp = "http://localhost:8080";
 					String itemName = m.getItemName();
 					double oldPrice = m.getPrice();
-					double newPrice = (double) updateMessage.get("price");
+					double newPrice = Double.parseDouble((String) updateMessage.get("price"));
 					String email = m.getEmail();
-					httpGet(gatewayIp,
-							String.format("/notification/price/%s/%s/$s", itemName, oldPrice, newPrice, email));
+					try {
+						httpGet(gatewayIp,
+								String.format("/notification/price/%s/%s/%s/%s", itemName, oldPrice, newPrice, email));
+					} catch (HttpClientErrorException e) {
+						System.err.println("Notificationservice offline; update successful");
+					}
 				}
 
 				// set new price
 				repository.delete(m);
-
-				m.setPrice((double) updateMessage.get("price"));
+				m.setPrice(Double.parseDouble((String) updateMessage.get("price")));
 				repository.save(m);
 			}
 		}
+		return result;
 	}
 
 }
