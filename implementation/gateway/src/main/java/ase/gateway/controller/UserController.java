4a0f1bfa8642df50a22cee84b7081abf6c44359e
Kevin Miller <a01638797@unet.univie.ac.at>
2021-06-05 21:34:07 +0200

    slightly reworked usercontroller

--- implementation/gateway/src/main/java/ase/gateway/controller/UserController.java
+++ implementation/gateway/src/main/java/ase/gateway/controller/UserController.java
@@ -1,38 +1,47 @@
 package ase.gateway.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import ase.gateway.user.User;
 import ase.gateway.user.UserRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-@Slf4j
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    //get User
-    @GetMapping("/{id}")
-    public User findById(@PathVariable("id")Long id){
-        return userRepository.findByUserId(id);
-    }
+	// get User
+	@GetMapping("/{id}")
+	public User findById(@PathVariable("id") Long id) {
+		return userRepository.findByUserId(id);
+	}
 
-    //add User
-    @PostMapping("/add")
-    public User saveDepartment(@RequestBody User new_user){
-        User newUser = new User(new_user.getEmail());
-        return userRepository.save(newUser);
-    }
+	// add User
+	@PostMapping("/add")
+	public User addOrReturnUser(@RequestBody User newUser) {
+		Optional<User> user = userRepository.findById(newUser.getId());
+		if (user.isPresent()) {
+			return user.get();
+		}
+		return userRepository.save(newUser);
+	}
 
-    //delete User
-    @RequestMapping(value = "/{id}")
-    @DeleteMapping
-    public String delete(@PathVariable("id") Long id) {
-
-        userRepository.deleteById(id);
-        return "Delete successfull";
-    }
+	// delete User
+	@RequestMapping(value = "/{id}")
+	@DeleteMapping
+	public String delete(@PathVariable("id") Long id) {
+		userRepository.deleteById(id);
+		return "Delete successfull";
+	}
 }
